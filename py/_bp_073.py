ae9d5b45e6ecb8d589c9b7fe9e14ea96e6b17076
mallox <mallox@gitee.local>
2026-09-07 17:18:52 +0800

    probe: 自动二分 round7(临时)

---
+++ py/_bp_073.py
@@ -0,0 +1,5 @@
+        if valid:
+            self.host = cached_host
+            self._speed_test_done = True
+
+    _CATEGORY_BLACKLIST = {'成人游戏', '漫画', '小说', '蜜穴女友', '一键脱衣', '春药商城', '同城交友', '吃瓜', '成人漫画'}
